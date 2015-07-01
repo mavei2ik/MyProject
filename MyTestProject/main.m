3da0b9de648b6b0609a68e1443ac82f7f467bd13
Anton Prokopets <prokav@gmail.com>
2015-07-01 22:08:04 +0300

    updated main file

--- MyTestProject/main.m
+++ MyTestProject/main.m
@@ -8,10 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-	    // insert code here...
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool
+	{
 	    NSLog(@"Hello, World!");
 	}
-    return 0;
+	
+	return 0;
 }
