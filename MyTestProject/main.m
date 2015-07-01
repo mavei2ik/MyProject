f3b3b6631b83e8355c3d0f54bf1c406fad852d59
Anton Prokopets <prokav@gmail.com>
2015-07-01 19:23:10 +0300

    Initial project commit.

---
+++ MyTestProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyTestProject
+//
+//  Created by Anton Prokopets on 7/1/15.
+//  Copyright (c) 2015 Anton Prokopets. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
